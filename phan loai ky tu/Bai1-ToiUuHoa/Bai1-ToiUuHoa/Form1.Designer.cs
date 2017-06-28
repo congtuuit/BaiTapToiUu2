55e796b06446cfbf0ecd0afd96e2785edb277703
congtuuit <congtuuit@gmail.com>
2017-06-28 21:50:56 +0700

    bt2

---
+++ phan loai ky tu/Bai1-ToiUuHoa/Bai1-ToiUuHoa/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Bai1_ToiUuHoa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox3 = new System.Windows.Forms.TextBox();
+            this.txtBox4 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(452, 216);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(100, 49);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "OPEN";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.txtBox1.Location = new System.Drawing.Point(12, 25);
+            this.txtBox1.Multiline = true;
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.ReadOnly = true;
+            this.txtBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBox1.Size = new System.Drawing.Size(421, 62);
+            this.txtBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(12, 117);
+            this.txtBox2.Multiline = true;
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.ReadOnly = true;
+            this.txtBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBox2.Size = new System.Drawing.Size(421, 62);
+            this.txtBox2.TabIndex = 6;
+            // 
+            // txtBox3
+            // 
+            this.txtBox3.Location = new System.Drawing.Point(12, 203);
+            this.txtBox3.Multiline = true;
+            this.txtBox3.Name = "txtBox3";
+            this.txtBox3.ReadOnly = true;
+            this.txtBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBox3.Size = new System.Drawing.Size(421, 62);
+            this.txtBox3.TabIndex = 7;
+            // 
+            // txtBox4
+            // 
+            this.txtBox4.Location = new System.Drawing.Point(12, 287);
+            this.txtBox4.Multiline = true;
+            this.txtBox4.Name = "txtBox4";
+            this.txtBox4.ReadOnly = true;
+            this.txtBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBox4.Size = new System.Drawing.Size(421, 62);
+            this.txtBox4.TabIndex = 8;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(452, 300);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(100, 49);
+            this.btn2.TabIndex = 9;
+            this.btn2.Text = "EXIT";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Bai1_ToiUuHoa.Properties.Resources._7consejos;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(570, 359);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.txtBox4);
+            this.Controls.Add(this.txtBox3);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(586, 398);
+            this.MinimumSize = new System.Drawing.Size(586, 398);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Classification Characters";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OpenFile;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox3;
+        private System.Windows.Forms.TextBox txtBox4;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ phan loai ky tu/Bai1-ToiUuHoa/Bai1-ToiUuHoa/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+
+namespace Bai1_ToiUuHoa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Kiểu chữ THƯỜNG";
+            label2.Text = "Kiểu chữ HOA";
+            label3.Text = "Kiểu SỐ";
+            label4.Text = "Kiểu khác";          
+        }
+
+        public void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFile.ShowDialog();
+            StreamReader txt = new StreamReader(OpenFile.FileName);
+            string a = txt.ReadToEnd();
+            txt.Close();
+            a = a.Replace(" ", "");
+            char[] array = a.ToCharArray();
+            List<string> Normal= new List<string>();//97-122
+            List<string> Capital=new List<string>();//65-90
+            List<string> Number=new List <string>();//48-57
+            List<string> Other=new List<string>(); 
+            // phan loai kieu  ky tu
+            for (int i=0; i< array.Length;i++)
+            {
+                int other=0;
+                int temp;
+                temp = (int)array[i];
+                //x = Convert.ToString(temp);
+                //MessageBox.Show(x);
+                if(temp>=97 && temp <=122)
+                {
+                    string flag = Convert.ToString(array[i]);
+                    Normal.Add(flag);
+                    other++;
+                }
+                if (temp>=65 && temp <= 90)
+                {
+                    string flag = Convert.ToString(array[i]);
+                    Capital.Add(flag);
+                    other++;
+                }
+                if (temp >=48 && temp <= 57)
+                {
+                    string flag = Convert.ToString(array[i]);
+                    Number.Add(flag);
+                    other++;
+                }
+                if (other ==0)
+                {
+                    string flag = Convert.ToString(array[i]);
+                    Other.Add(flag);
+                }
+            }
+          //txt.Close();
+            MessageBox.Show("Bắt đầu phân loại...", "Thông báo!");
+          string abc="";
+          string hoa = "";
+          string number = "";
+          string khac = "";
+            for (int i=0; i< Normal.Count;i++)
+            {
+                abc += Normal[i];
+                
+            }
+            for (int i = 0; i < Capital.Count; i++)
+            {
+                hoa += Capital[i];
+            }
+            for (int i = 0; i < Number.Count; i++)
+            {
+                number += Number[i];
+            }
+            for (int i = 0; i < Other.Count;i++ )
+            {
+                khac += Other[i];
+            }
+                txtBox1.Text = abc;
+                txtBox2.Text = hoa;
+                txtBox3.Text = number;
+                txtBox4.Text = khac;
+         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+     }
+}
